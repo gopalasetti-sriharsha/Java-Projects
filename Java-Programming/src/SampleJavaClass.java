fd4858b4567a10f96363f6b54a90e472742a99a1
Sumanth Penta <sumanth.penta@gmail.com>
2020-09-02 00:09:34 +0530

    New line added

--- Java-Programming/src/SampleJavaClass.java
+++ Java-Programming/src/SampleJavaClass.java
@@ -1,5 +1,6 @@
 public class SampleJavaClass {
     public static void main(String args[]){
         System.out.println("This is a sample code output");
+        System.out.println("This is another line");
     }
 }
