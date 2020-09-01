9a70ec790eaf0e4ae5f27aa8c120d586c6bee5a1
Sumanth Penta <sumanth.penta@gmail.com>
2020-09-02 00:28:54 +0530

    removed unwanted fiels

--- Java-Programming/src/SampleJavaClass.java
+++
@@ -1,6 +0,0 @@
-public class SampleJavaClass {
-    public static void main(String args[]){
-        System.out.println("This is a sample code output");
-        System.out.println("This is another line");
-    }
-}
